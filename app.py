c1a6b0d591d3165c7383ab3cf0160fe742d65d9b
OswaldoGomezBellota <33400345+OswaldoGomezBellota@users.noreply.github.com>
2026-03-04 20:42:29 -0500

    Finish EDA and dashboard

--- app.py
+++ app.py
@@ -2,20 +2,21 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.header("Dashboard de anuncios de venta de vehículos")
-
+# Leer dataset
 car_data = pd.read_csv("vehicles_us.csv")
-
+# Crear encabezado del dashboard
+st.header("Dashboard de anuncios de venta de vehículos")
+# Mostrar vista previa del dataset
 st.write("Vista previa del dataset:")
 st.dataframe(car_data.head())
-
+# Botón para construir histograma del odómetro
 hist_button = st.button("Construir histograma del odómetro")
-
+# Si el botón es presionado, mostrar el histograma
 if hist_button:
     st.write("Histograma del odómetro")
     fig_hist = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig_hist, use_container_width=True)
-
+# Botón para construir gráfico de dispersión Precio vs Odómetro
 scatter_button = st.button("Construir gráfico de dispersión Precio vs Odómetro")
 
 if scatter_button:
